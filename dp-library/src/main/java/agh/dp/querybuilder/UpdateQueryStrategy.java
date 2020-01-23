cb484f1d8b696208eb2fe9580ef0168bcb686e98
mw1213 <wilk.maciejcyryl@gmail.com>
2020-01-23 12:38:10 +0100

    getTableNamesFromQuery now properly returns name

--- dp-library/src/main/java/agh/dp/querybuilder/UpdateQueryStrategy.java
+++ dp-library/src/main/java/agh/dp/querybuilder/UpdateQueryStrategy.java
@@ -9,41 +9,30 @@ import java.util.regex.Pattern;
 
 public class UpdateQueryStrategy implements QueryStrategy {
 
-    private List<String[]> getTableNames(String startingQuery){
-        List<String[]> names = new ArrayList<>();
+    private List<String> getTableNames(String startingQuery){
+        List<String> names = new ArrayList<>();
         StringBuilder builder = new StringBuilder(startingQuery);
         Pattern fromPattern = Pattern.compile("(update)", Pattern.CASE_INSENSITIVE);
         Pattern endingOfTableNames = Pattern.compile("(set)", Pattern.CASE_INSENSITIVE);
         Matcher matcher1 = fromPattern.matcher(builder);
         Matcher whereMatcher = endingOfTableNames.matcher(builder);
-        int start;
+        int start, end;
+        String name;
         matcher1.find();
         start = matcher1.end()+1;
         whereMatcher.find();
-        findNewName(names, builder, whereMatcher, start);
+        end = whereMatcher.start();
+        name = builder.substring(start,end);
+        name = name.trim();
+        String[] splitted = name.split(" ");
+        names.add(splitted[0]);
         return names;
-    }
-
-    private void findNewName(List<String[]> names, StringBuilder builder, Matcher matcher, int start) {
-        int end;
-        String name;
-        String[] names2;
-        if(matcher.find()){
-            end = matcher.start();
-            name = builder.substring(start,end);
-            name = name.trim();
-            names2 = name.split(" ");
-            if (names2.length != 2) {
-                names.set(1, names.get(0));
-            }
-            names.add(names2);
-        }
     }
 
     public String buildQuery(String startingQuery, List<Permission> permissions){
         StringBuilder builder = new StringBuilder(startingQuery);
 
-        List<String[]> tableNames = getTableNames(startingQuery);
+        List<String> tableNames = getTableNames(startingQuery);
 
         Pattern wherePattern = Pattern.compile("where", Pattern.CASE_INSENSITIVE);
         Matcher whereMatcher = wherePattern.matcher(builder);
@@ -68,14 +57,14 @@ public class UpdateQueryStrategy implements QueryStrategy {
         return builder.toString();
     }
 
-    private void makeConstraints(Map<String, List<Long>> permissions, StringBuilder builder, List<String[]> tableNames) {
+    private void makeConstraints(Map<String, List<Long>> permissions, StringBuilder builder, List<String> tableNames) {
         List<Long> perms;
-        for (String[] tableName : tableNames) {
-            perms = permissions.getOrDefault(tableName[0], null);
+        for (String tableName : tableNames) {
+            perms = permissions.getOrDefault(tableName, null);
             if (perms == null) {
                 continue;
             }
-            builder.append(tableName[1]).append(".ID IN (");
+            builder.append(tableName).append(".ID IN (");
             for (long number : perms) {
                 builder.append(number).append(", ");
             }
@@ -87,20 +76,16 @@ public class UpdateQueryStrategy implements QueryStrategy {
     }
 
     public List<String> getTableNamesFromQuery(String query){
-        List<String[]> tableNamePairs = getTableNames(query);
-        List<String> officialTableNames = new ArrayList<>();
-        for (String[] tableNamePair : tableNamePairs){
-            officialTableNames.add(tableNamePair[0]);
-        }
-        return officialTableNames;
+        List<String> tableNames = getTableNames(query);
+        return tableNames;
     }
 
 
-//    public static void main(String[] args) {
-//        String s = "UPDATE table_name tab set column1 = value1, column2 = value2, ... WHERE condition;";
-//        UpdateQueryStrategy queryBuilder = new UpdateQueryStrategy();
-//        Permission permission = new Permission("table_name", PermissionsProvider.UPDATE, (long)1, (long)1);
-//        String s2 = queryBuilder.buildQuery(s, Collections.singletonList(permission));
-//        System.out.println(s2);
-//    }
+    public static void main(String[] args) {
+        String s = "update Student set FIRSTNAME=?, LASTNAME=? where id=?";
+        UpdateQueryStrategy queryBuilder = new UpdateQueryStrategy();
+        Permission permission = new Permission("table_name", PermissionsProvider.UPDATE, (long)1, (long)1);
+        String s2 = queryBuilder.buildQuery(s, Collections.singletonList(permission));
+        System.out.println(s2);
+    }
 }
